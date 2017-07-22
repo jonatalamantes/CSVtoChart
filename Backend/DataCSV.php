37a9d90cb277c97ad47ff16fc4c47ec61fc385c4
Jonathan E. Sandoval Talamantes (Jesxtra) <jonathan_s_pisis@hotmail.com>
2017-07-21 20:06:31 -0500

    Cambio de libreria en Graficas

--- Backend/DataCSV.php
+++ Backend/DataCSV.php
@@ -5,6 +5,11 @@
 	ini_set("memory_limit", "64G");
 	ini_set("post_max_size", "8G");
 
+    require_once(__DIR__."/../Third-Party/jpgraph/src/jpgraph.php");
+    require_once(__DIR__."/../Third-Party/jpgraph/src/jpgraph_pie.php");
+    require_once(__DIR__."/../Third-Party/jpgraph/src/jpgraph_bar.php");
+    require_once(__DIR__."/../Third-Party/jpgraph/src/jpgraph_pie3d.php");
+
 	class DataCSV
 	{
 		private $instances;
@@ -272,7 +277,10 @@
 
 			    	for ($i = 0; $i < $maxInstances; $i++) 
 					{
-			            fputcsv($handle, $temp2[$i]);
+                        if (array_key_exists($i, $temp2))
+                        {
+                            fputcsv($handle, $temp2[$i]);
+                        }
 			        }
 		    	}
 
@@ -285,7 +293,7 @@
 		    }
 		}
 
-		static function FrecuenceTableToD3Chart($tf, $name = "", $tipo = "")
+		static function FrecuenceTableToD3Chart($tf, $sort = "", $name = "")
 		{
 			if ($tf == NULL || empty($tf))
 			{
@@ -294,6 +302,13 @@
 
 			if (sizeof($tf["statics"]) > 10 || $tipo == "bar")
 			{
+				if ($sort !== "")
+				{
+					$tfTemp = $tf;
+					DataCSV::sortBySubkey($tfTemp["statics"], $sort);					
+					return DataCSV::FrecuencesTableToD3BarChart($tfTemp, $name);
+				}
+
 				return DataCSV::FrecuencesTableToD3BarChart($tf, $name);
 			}
 			else
@@ -313,79 +328,43 @@
 
 			if ($nameFile !== "NF")
 			{
-				$colorHash = array("#396AB1", "#DA7C30", "#3E9651", "#CC2529", 
-								   "#535154", "#6B4C9A", "#922428", "#CCC210", 
-								   "#808585", "#84BA64");
+                $keys   = array();
+                $values = array();
 
-				$script  = "<svg width='960' height='500' id='ft-$nameFile'></svg>";
-				$script .= "<script>";
-				$script .= "
-					var svg$nameFile = d3.select('#ft-$nameFile'),
-					    width$nameFile = +svg$nameFile.attr('width'),
-					    height$nameFile = +svg$nameFile.attr('height'),
-					    radius$nameFile = Math.min(width$nameFile, height$nameFile) / 2,
-					    g$nameFile = svg$nameFile.append('g')
-					    		 		.attr('transform', 'translate(' + width$nameFile / 3.5 + ',' + height$nameFile / 2 + ')');\n";
+                foreach ($tf["statics"] as $key => $value) 
+                {
+                    $keys[]   = $value["key"] . ": " . $value["value"];;
+                    $values[] = $value["value"];
+                }
 
-				$script .= " var color$nameFile = d3.scaleOrdinal([";
+                // A new pie graph
+                $graph = new PieGraph(960,600);
+                $graph->SetShadow();
+                $graph->title->Set($tf["atribute"]);
+                              
+                // Setup the pie plot
+                $p1 = new PiePlot($values);
+                 
+                // Adjust size and position of plot
+                $p1->SetSize(0.3);
+                $p1->SetCenter(0.1,-0.52);
+                 
+                // Setup slice labels and move them into the plot
+                $p1->value->SetFont(FF_FONT2,FS_BOLD);
+                $p1->value->SetColor("black");
+                $p1->SetLabelType(PIE_VALUE_ABS);
+                $p1->SetLabels($keys, 1); 
+                $p1->value->Show(); 
+                 
+                // Finally add the plot
+                $graph->Add($p1);                 
+                $gd = $graph->Stroke(_IMG_HANDLER);
 
-				$counter = 0;
-				foreach ($tf["statics"] as $key => $value) 
-				{
-					$script .= "'" . $colorHash[$counter] . "',";
-					$counter++;
-				}
+                $file =  __DIR__."/../App/Charts/$nameFile.png";
+                imagepng($gd, $file);
+                imagedestroy($gd);
 
-				$script = substr($script, 0, strlen($script)-1);
-
-				$script .= "]); ";
-
-				$script .= "
-					var pie$nameFile = d3.pie()
-					    .sort(null)
-					    .value(function(d) { return d.value; });
-
-					var path$nameFile = d3.arc()
-					    .outerRadius(radius$nameFile - 10)
-					    .innerRadius(0);
-
-					var label$nameFile = d3.arc()
-					    .outerRadius(radius$nameFile - 40)
-					    .innerRadius(radius$nameFile - 40);
-
-					d3.csv('Frecuences/$nameFile.csv', function(d) {
-					  d.value = +d.value;
-					  return d;
-					}, function(error, data) {
-					  if (error) throw error;
-
-					  var arc$nameFile = g$nameFile.selectAll('.arc')
-					    .data(pie$nameFile(data))
-					    .enter().append('g')
-					      .attr('class', 'arc');
-
-					  arc$nameFile.append('path')
-					      .attr('d', path$nameFile)
-					      .attr('dx', '0px')
-					      .attr('fill', function(d) { return color$nameFile(d.data.value); });
-
-					  arc$nameFile.append('rect')
-					  	  .attr('x', '290px')
-					      .attr('y', function(d) { return (d.index * 50 - 240) + 'px'})
-					      .attr('width', '290px')
-					      .attr('height', '30px')
-					      .attr('stroke', function(d) { return color$nameFile(d.data.value); })
-					      .attr('stroke-width', '5')
-					      .attr('fill', 'white')
-
-					  arc$nameFile.append('text')
-					  	  .attr('dx', '300px')
-					      .attr('dy', function(d) { return (d.index * 50 - 220) + 'px'})
-					      .attr('font-size', '20px')
-					      .attr('fill', 'black')
-					      .text(function(d) { return d.data.key + ' => ' + d.data.value; });
-					});
-					</script>";
+				$script .= "<img src='Charts/$nameFile.png'>";
 
 				return $script;
 			}
@@ -397,77 +376,79 @@
 
 		static function FrecuencesTableToD3BarChart($tf, $name)
 		{
-			if ($tf == NULL || empty($tf))
-			{
-				return "";
-			}
+            if ($tf == NULL || empty($tf))
+            {
+                return "";
+            }
 
-			$nameFile = DataCSV::FrecuenceTableToFileCSV($tf, $name, 20);
+            $nameFile = DataCSV::FrecuenceTableToFileCSV($tf, $name);
 
-			if ($nameFile !== "NF")
-			{
-				$colorHash = array("#396AB1", "#DA7C30", "#3E9651", "#CC2529", 
-								   "#535154", "#6B4C9A", "#922428", "#CCC210", 
-								   "#808585", "#84BA64");
+            if ($nameFile !== "NF")
+            {
+                $keys   = array();
+                $values = array();
 
-				$script  = "<svg width='960' height='500' id='ft-$nameFile'></svg>";
-				$script .= "<script>";
-				$script .= "
-					var svg$nameFile = d3.select('#ft-$nameFile'),
-					    margin$nameFile = {top: 20, right: 50, bottom: 30, left: 60},
-					    width$nameFile = +svg$nameFile.attr('width') - margin$nameFile.left - margin$nameFile.right,
-					    height$nameFile = +svg$nameFile.attr('height') - margin$nameFile.top - margin$nameFile.bottom;
+                foreach ($tf["statics"] as $key => $value) 
+                {
+                    $keys[]   = $value["key"];
+                    $values[] = $value["value"];
+                }
 
-					var x$nameFile = d3.scaleBand().rangeRound([0, width$nameFile]).padding(0.1),
-					    y$nameFile = d3.scaleLinear().rangeRound([height$nameFile, 0]);
+                // A new pie graph
+                $graph = new Graph(960,600);
+                $graph->SetShadow();
+				$graph->SetScale('textlin');
+				$graph->graph_theme = null;
+				$graph->SetFrame(false);
+                $graph->xaxis->SetTickLabels($keys); 
+                $graph->title->Set($tf["atribute"]);				
 
-					var g$nameFile = svg$nameFile.append('g')
-					    .attr('transform', 'translate(' + margin$nameFile.left + ',' + margin$nameFile.top + ')');
+                // Setup the pie plot
+                $p1 = new BarPlot($values);
+                $p1->SetFillColor("green");
+                $p1->value->Show();
+                $p1->value->SetColor("black");
+                $p1->value->SetFont( FF_FONT1, FS_BOLD);
+                $p1->value->SetFormat( "%0.1f");
 
-					d3.csv('Frecuences/$nameFile.csv', function(d) {
-					  d.value = +d.value;
-					  return d;
-					}, function(error, data) {
-					  if (error) throw error;
+                // Finally add the plot
+                $graph->Add($p1);                 
+                $gd = $graph->Stroke(_IMG_HANDLER);
 
-					  x$nameFile.domain(data.map(function(d) { return d.key; }));
-					  y$nameFile.domain([0, d3.max(data, function(d) { return d.value; })]);
+                $file =  __DIR__."/../App/Charts/$nameFile.png";
+                imagepng($gd, $file);
+                imagedestroy($gd);
 
-					  g$nameFile.append('g')
-					      .attr('class', 'axis axis--y')
-					      .call(d3.axisLeft(y$nameFile).ticks(10, '%'))
-					    .append('text')
-					      .attr('transform', 'rotate(-90)')
-					      .attr('y', 6)
-					      .attr('dy', '0.71em')
-					      .attr('text-anchor', 'end')
-					      .text('Value');
+                $script .= "<img src='Charts/$nameFile.png'>";
 
-					  g$nameFile.selectAll('.bar')
-					    .data(data)
-					    .enter().append('rect')
-					      .attr('class', 'bar')
-					      .attr('x', function(d) { return x$nameFile(d.key); })
-					      .attr('y', function(d) { return y$nameFile(d.value); })
-					      .attr('width', x$nameFile.bandwidth())
-					      .attr('fill', 'steelblue')
-					      .attr('height', function(d) { return height$nameFile - y$nameFile(d.value); });
+                return $script;
+            }
+            else
+            {
+                return "";
+            }
+        }
 
-					  g$nameFile.append('g')
-					      .attr('class', 'axis axis--x')
-					      .attr('transform', 'translate(0,' + height$nameFile + ')')
-					      .call(d3.axisBottom(x$nameFile));
+       /** 
+        * Sort one associative array by one subkey
+        *
+        * @author Jonathan Sandoval    <jonathan_s_pisis@hotmail.com>
+        * @param  &array        array reference
+        * @param  string        subkey name
+        * @param  typeSort      constant of the type of sort
+        */
+        static function sortBySubkey(&$array = null, $subkey = "", $sortType = SORT_ASC) 
+        {
+            if ($array !== NULL)
+            {
+                foreach ($array as $subarray) 
+                {
+                    $keys[] = $subarray[$subkey];
+                }
 
-					});
-					</script>";
-
-				return $script;
-			}
-			else
-			{
-				return "";
-			}
-		}
+                array_multisort($keys, $sortType, $array);
+            }
+        }
 
 		static function randomTime()
 		{
